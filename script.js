3aaddbb64a6a471f2a595aa7ce86e22163b8068e
elakuladajefula <ela.wisniewska3@wp.pl>
2023-08-19 18:02:51 +0200

    Added gallery slider

--- script.js
+++ script.js
@@ -10,12 +10,26 @@ function closeSearchbar()
     document.getElementById("searchbar").style.display = "none";
 }
 
-function toggleSlider(slider)
+function toggleSlider(slider, id)
 {
     let displayValue = (document.getElementById(slider).style.display === "none") 
         ? "block" : "none";
     document.getElementById(slider).style.display = displayValue;
     document.getElementById(slider + "SliderClose").style.display = displayValue;
+    if (id !== '')
+    {
+        if (id == 0)
+        {
+            for (var i = 0; i < 18; i++)
+            {
+                document.getElementById("slide" + (i + 1)).classList.remove("active");
+            }
+        }
+        else
+        {
+            document.getElementById("slide" + id).classList.add("active");
+        }
+    }
 }
 
 function showMore()
